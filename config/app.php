fcb1d42822d630fa5db91ffe5ff84d088a52ca07
Wladimir Andrea <wladimirandrea2@gmail.com>
2022-12-04 18:26:59 -0500

    8mo video

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Cart' => Gloudemans\Shoppingcart\Facades\Cart::class,
     ])->toArray(),
 
 ];
