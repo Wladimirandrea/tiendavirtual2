7a9bbb35e17fae34435707bb73c7029953506d97
Wladimir Andrea <wladimirandrea2@gmail.com>
2022-12-04 18:40:25 -0500

    9no video

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQuantity($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+        $this->emitTo('cart-icon-component', 'refreshComponent');
+    }
+    public function decreaseQuantity($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+        $this->emitTo('cart-icon-component', 'refreshComponent');
+    }
+    public function destroy($id){
+        Cart::remove($id);
+        session()->flash('success_message', 'Producto ha sido removido del carrito');
+        $this->emitTo('cart-icon-component', 'refreshComponent');
+    }
+    public function clearAll(){
+        Cart::destroy();
+        $this->emitTo('cart-icon-component', 'refreshComponent');
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
