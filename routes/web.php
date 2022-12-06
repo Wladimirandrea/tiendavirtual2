720fba74f26ff4fc9fa1bf890bd499ed0541a4d4
Wladimir Andrea <wladimirandrea2@gmail.com>
2022-12-06 08:32:20 -0500

    14 video

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
+use App\Http\Livewire\SearchComponent;
+
 
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
@@ -33,6 +35,7 @@ Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 Route::get('/cart', CartComponent::class)->name('shop.cart');
 Route::get('/checkout', CheckoutComponent::class)->name('shop.checkout');
 Route::get('/product-category/{slug}', CategoryComponent::class)->name('product.category');
+Route::get('/search', SearchComponent::class)->name('product.search');
 /*
 
 Route::get('/dashboard', function () {
